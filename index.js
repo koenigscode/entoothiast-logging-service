9ef9358a457121f9965314c79975fbe5e972ef42
Michael Koenig <koenigmichael2002@gmail.com>
2023-12-19 19:43:54 +0100

    Implement basic logging #2
    - Log topic
    - Log payload (message)

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 import * as mqtt from "mqtt"
+import PGClient from "pg-native"
+
+const db = new PGClient()
+db.connectSync(process.env.CONNECTION_STRING)
 
 const client = mqtt.connect(process.env.BROKER_URL)
+
 console.log(`Broker URL: ${process.env.BROKER_URL}`)
 
-client.on("connect", () => {
+client.on("connect", async () => {
     console.log("logging-service connected to broker")
-    client.subscribe("test", (err) => {
-      if (!err) {
-        client.publish("test", "Hello from logging-service");
-      }
+    client.subscribe("#", (err) => {
+        if (err) {
+            console.log(err)
+        }
     });
-  });
+});
 
-client.on("message", (topic, message) => {
-  console.log(`received message: topic ${topic}, message: ${message.toString()}`)
-})
+
+client.on('message', (topic, payload) => {
+    payload = JSON.parse(payload)
+    delete payload.password
+    payload = JSON.stringify(payload)
+
+    try {
+        db.querySync("insert into public.logs (topic, payload) values ($1, $2)", [topic, payload])
+        console.log(`Logged message on topic ${topic}`)
+    } catch (e) {
+        console.log(e)
+    }
+});
+
+client.on("error", (error) => {
+    console.error("Error connecting to the broker:", error);
+});
+
+process.on('SIGINT', () => {
+    client.end();
+    console.log('Disconnected from MQTT broker');
+    process.exit();
+});

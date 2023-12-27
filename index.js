a69755930ffda39ddfcec69b44a46c6e9b4545a4
Michael Koenig <koenigmichael2002@gmail.com>
2023-12-27 17:57:17 +0100

    Implement log query functionality #3

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import * as mqtt from "mqtt"
+import WhitelistMqttRequest from "./WhitelistMqttRequest.js";
 import PGClient from "pg-native"
 
 const db = new PGClient()
 db.connectSync(process.env.CONNECTION_STRING)
 
 const client = mqtt.connect(process.env.BROKER_URL)
+
+export const mqttReq = new WhitelistMqttRequest(client, ["v1/logging/read"]);
+
 
 console.log(`Broker URL: ${process.env.BROKER_URL}`)
 
@@ -21,15 +25,49 @@ client.on("connect", async () => {
 client.on('message', (topic, payload) => {
     payload = JSON.parse(payload)
     delete payload.password
+    if (payload.hasOwnProperty("logs")) {
+        payload.logs = "[Truncated]"
+    }
     payload = JSON.stringify(payload)
 
     try {
         db.querySync("insert into public.logs (topic, payload) values ($1, $2)", [topic, payload])
-        console.log(`Logged message on topic ${topic}`)
+        console.log(`Logged: topic: "${topic}" payload: "${payload}"`)
     } catch (e) {
         console.log(e)
     }
 });
+
+
+mqttReq.response("v1/logging/read", payload => {
+    payload = JSON.parse(payload)
+
+    let query = "select id, topic, payload from public.logs order by id desc";
+    let params = [];
+
+    if (payload.offset) {
+        if (parseInt(payload.offset) < 0) {
+            return JSON.stringify({ httpStatus: 400, message: "Offset must be >= 0 or unset" })
+        }
+        query += " offset $1";
+        params.push(payload.offset);
+    }
+
+    if (payload.limit) {
+        if (parseInt(payload.limit) < 1) {
+            return JSON.stringify({ httpStatus: 400, message: "Limit must be >=1 or unset" })
+        }
+        query += " limit $" + (params.length + 1);
+        params.push(payload.limit);
+    }
+
+    try {
+        const logs = db.querySync(query, params);
+        return JSON.stringify({ httpStatus: 200, logs });
+    } catch {
+        return JSON.stringify({ httpStatus: 500, message: "Some error occurred" });
+    }
+})
 
 client.on("error", (error) => {
     console.error("Error connecting to the broker:", error);
